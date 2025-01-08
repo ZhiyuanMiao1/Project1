ea31c9b1857f4cf9970981dcc0cbb87b36761db8
Zhiyuan Miao <zhiyuanmiao3@gmail.com>
2025-01-08 20:07:20 +0800

    Frontend

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,42 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Register from './components/Register';
 import Login from './components/Login';
 import './App.css';
 
-function App() {
+// 创建一个 Home 组件，作为默认的主页
+function Home() {
     return (
-        <Router>
-            <div className="App">
-                <header className="App-header">
-                    {/* 你可以保留这个 Logo 或移除 */}
-                    <p>欢迎来到我的应用!</p>
-                </header>
-                <Routes>
-                    <Route path="/register" element={<Register />} />
-                    <Route path="/login" element={<Login />} />
-                </Routes>
-            </div>
-        </Router>
+        <div>
+            <h1>欢迎来到我的应用!</h1>
+            <p>请点击上方链接导航到登录或注册页面。</p>
+        </div>
     );
 }
+
+function App() {
+  return (
+      <Router>
+          <div className="App">
+              <header className="App-header">
+                  <p>欢迎来到我的应用!</p>
+                  <nav>
+                      <Link to="/">主页</Link> |{' '}
+                      <Link to="/register">注册</Link> |{' '}
+                      <Link to="/login">登录</Link>
+                  </nav>
+              </header>
+              <div className="App-content">
+                  <Routes>
+                      <Route path="/" element={<Home />} />
+                      <Route path="/register" element={<Register />} />
+                      <Route path="/login" element={<Login />} />
+                  </Routes>
+              </div>
+          </div>
+      </Router>
+  );
+}
+
 
 export default App;
