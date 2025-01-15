b21f6237871950cd6ccc6fb8d49c5bc7622efc19
Zhiyuan Miao <zhiyuanmiao3@gmail.com>
2025-01-15 17:13:38 +0800

    Frontend

--- my-app/src/components/RegisterPopup/RegisterPopup.js
+++ my-app/src/components/RegisterPopup/RegisterPopup.js
@@ -11,6 +11,15 @@ const RegisterPopup = ({ onClose }) => {
         <h2>注册</h2>
         <div className="register-modal-divider"></div>
         <h3>MentorX欢迎您</h3>
+        <div className="register-input-area">
+          <input type="email" placeholder="请输入邮箱" className="register-input" />
+          <input type="password" placeholder="请输入密码" className="register-input" />
+          <input type="password" placeholder="请确认密码" className="register-input" />
+        </div>
+        <div className="register-button-group">
+          <button className="register-button student-button">我是学生</button>
+          <button className="register-button teacher-button">我是教师（需审核）</button>
+        </div>
       </div>
     </div>
   );
